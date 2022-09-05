1eadba97591f077f93e9b524a1c777868fec33d0
obov <obov3e@gmail.com>
2022-09-05 21:45:37 +0900

    validation oninput

--- static/js/header.js
+++ static/js/header.js
@@ -2,6 +2,13 @@ const TAG_ID = {
   SIGN: "sign",
   SIGN_IN: "signIn",
   SIGN_UP: "signUp",
+  SIGN_UP_ID: "signUpId",
+  SIGN_UP_PW: "signUpPw",
+  SIGN_UP_CONFIRM: "signUpConfirm",
+  SIGN_UP_ERROR_MESSAGE: "signUpErrorMessage",
+  SIGN_IN_ID: "signInId",
+  SIGN_IN_PW: "signInPw",
+  SIGN_IN_ERROR_MESSAGE: "signInErrorMessage",
 };
 
 const sign = () => $("#" + TAG_ID.SIGN);
@@ -27,16 +34,43 @@ const hideSign = function () {
   signIn().hide();
   signUp().hide();
 };
+const handleClickReview = function () {
+  loadRev();
+};
+const handleClickLogo = function () {
+  loadHome();
+};
+
+const signUpId = () => $("#" + TAG_ID.SIGN_UP_ID);
+const signUpPw = () => $("#" + TAG_ID.SIGN_UP_PW);
+const signUpConfirm = () => $("#" + TAG_ID.SIGN_UP_CONFIRM);
+const signUpErrorMessage = () => $("#" + TAG_ID.SIGN_UP_ERROR_MESSAGE);
+const signInId = () => $("#" + TAG_ID.SIGN_IN_ID);
+const signInPw = () => $("#" + TAG_ID.SIGN_IN_PW);
+const signInErrorMessage = () => $("#" + TAG_ID.SIGN_IN_ERROR_MESSAGE);
+
 const handleSubmitSignIn = function (event) {
   event.preventDefault();
-  $.ajax({
-    url: "/signin",
-    data: {},
-    method: "POST",
-    success: function (res) {
-      console.log(res);
-    },
-  });
+  const username = signInId().val();
+  const password = signInPw().val();
+  const isIdValid = reg.id.test(username);
+  const isPwValid = reg.password.test(password);
+  if (isIdValid && isPwValid) {
+    $.ajax({
+      url: "/signin",
+      data: { username, password },
+      method: "POST",
+      success: function (res) {
+        console.log(res);
+      },
+    });
+  } else if (!isIdValid && !isPwValid) {
+    signInErrorMessage().text = "id pw error";
+  } else if (!isIdValid) {
+    signInErrorMessage().text = "id error";
+  } else {
+    signInErrorMessage().text = "pw error";
+  }
 };
 const handleSubmitSignUp = function (event) {
   event.preventDefault();
@@ -49,10 +83,46 @@ const handleSubmitSignUp = function (event) {
     },
   });
 };
-const handleClickReview = function () {
-  console.log("review");
-  loadRev();
+
+const validateSignUp = function () {
+  const idVal = signInId().val();
+  const pwVal = signInPw().val();
+  const confirmVal = signUpConfirm().val();
+  if (!reg.id.test(idVal) && idVal !== "") {
+    signUpErrorMessage().text("id error");
+  } else if (!reg.id.test(pwVal) && pwVal !== "") {
+    signUpErrorMessage().text("pw error");
+  } else if (pwVal !== confirmVal) {
+    signUpErrorMessage().text("confirm error");
+  } else {
+    signUpErrorMessage().text("");
+  }
 };
-const handleClickLogo = function () {
-  loadHome();
+const validateSignIn = function () {
+  const idVal = signInId().val();
+  const pwVal = signInPw().val();
+  if (!reg.id.test(idVal) && idVal !== "") {
+    signInErrorMessage().text("id error");
+  } else if (!reg.id.test(pwVal) && pwVal !== "") {
+    signInErrorMessage().text("pw error");
+  } else {
+    signInErrorMessage().text("");
+  }
+};
+
+const handleInputSignUpId = function () {
+  validateSignUp();
+};
+const handleInputSignUpPw = function () {
+  validateSignUp();
+};
+const handleInputSignUpConfirm = function () {
+  validateSignUp();
+};
+
+const handleInputSignInId = function (event) {
+  validateSignIn();
+};
+const handleInputSignInPw = function (event) {
+  validateSignIn();
 };
---
+++ static/js/modules/validation.js
@@ -0,0 +1,6 @@
+const reg = {
+  id: /^(?=.*[a-zA-Z])[-a-zA-Z0-9]{3,15}$/,
+  password: /^(?=.*[a-zA-Z])[-a-zA-Z0-9]{8,15}$/,
+  title: /^(?=.*[a-zA-Z])[-a-zA-Z0-9]{3,30}$/,
+  content: /^(?=.*[a-zA-Z])[-a-zA-Z0-9]{3,300}$/,
+};
