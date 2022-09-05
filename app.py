b31e3e10436528d7b407b3f7026f2d53aa5a7127
ninthsun91 <ninthsun91@gmail.com>
2022-09-05 18:39:00 +0900

    build api structure for movie/review/signin/signup

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -29,7 +30,7 @@ def home():
 
 @app.route("/movie", methods=["GET"])
 def get_movie():
-   #  code = request.args["code"]
+   # code = int(request.args["code"])
    code = 999999
    movie = db.movies.find_one({"code": code}, {"_id": False})
 
@@ -46,12 +47,36 @@ def get_movie():
 
 @app.route("/review", methods=["GET"])
 def get_review():
-   return "show review"
+   # code = int(request.args["code"])
+   code = 999999
+   movie = db.movies.find_one({"code": code}, {"_id": False})
+
+   rids = movie["reviews"]
+   reviews = []
+   for rid in rids:
+      reviews.append(db.reviews.find_one({"_id": ObjectId(rid)}, {"_id": False}))
+
+   return jsonify({ reviews })
 
 
 @app.route("/review", methods=["POST"])
 def post_review():
-   return "post review"
+   code = int(request.form["code"])
+   username = request.form["username"]
+   comment = request.form["comment"]
+   userRating = request.form["userRating"]
+   time = str(datetime.now()).split(".")[0]
+
+   review = {
+      "code": code,
+      "username": username,
+      "comment": comment,
+      "userRating": userRating,
+      "time": time,
+   }
+   db.reviews.insert_one(review)
+
+   return jsonify({"msg": "리뷰를 등록했습니다!"})
 
 
 @app.route("/sign_in")
@@ -61,7 +86,22 @@ def components_sign_in():
 
 @app.route("/sign_in", methods=["POST"])
 def sign_in():
-   return "sign in"
+   username = request.form["username"]
+   password = request.form["password"]
+   password_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+   user = db.users.find_one({"username": username, "password": password_hash})
+   if user is not None:
+      uid = user["uid"]
+      payload = {
+         "uid": uid,
+         "username": username,
+         "exp": datetime.utcnow() + timedelta(seconds = 60*60)
+      }
+      token = jwt.encode(payload, KEY, algorithm="HS256") #.decode("utf-8")   # annotate while using localhost
+      return jsonify({"result": True, "logintoken": token})
+   else:
+      return jsonify({"result": False, "msg": "아이디, 비밀번호가 틀렸습니다."})
 
 
 @app.route("/sign_up")
@@ -71,7 +111,22 @@ def components_sign_up():
 
 @app.route("/sign_up", methods=["POST"])
 def sign_up():
-   return "sign up"
+   username = request.form["username"]
+   password = request.form["password"]
+   password_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+   cnt = db.users.find_one({}, {"_id": False})
+   uid = cnt["cnt"] + 1
+
+   profile = {
+      "uid": uid,
+      "username": username,
+      "password": password_hash,
+      "reviews": []
+   }
+   db.users.insert_one(profile)
+
+   return redirect(url_for("/sign_in"))
 
 
 if __name__ == "__main__":
