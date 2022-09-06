ce114c74bc88a06fe54ee46f15d19718b565eb06
ninthsun91 <ninthsun91@gmail.com>
2022-09-06 16:53:45 +0900

    add movies to DB / auto DB update / change review likes by uid

--- movie.py
+++ movie.py
@@ -6,6 +6,9 @@ import json
 import os
 from dotenv import load_dotenv
 
+from bs4 import BeautifulSoup
+import requests
+
 from regex import *
 
 load_dotenv()
@@ -31,10 +34,15 @@ def movie_view():
 
 @movie_bp.route("/search", methods=["POST"])
 def search_title():
-   keyword = request.form["keyword"]   
-   result = search_db(keyword) + search_naver(keyword)
+    keyword = request.form["keyword"]
 
-   return jsonify({ result })
+    naver = search_naver(keyword)    
+    for n in naver:
+        n.pop("image")
+        n.pop("naverRating")
+    result = search_db(keyword) + naver
+
+    return jsonify({ "result": result })
 
 
 # MongoDB 제목 검색
@@ -54,7 +62,8 @@ def search_db(keyword):
             "code": 1,
             "title": 1,
             "director": 1,
-            "pubDate": 1
+            "actor": 1,
+            "pubDate": 1,
          }
         }
    ]
@@ -67,31 +76,68 @@ def search_db(keyword):
 
 # 네이버영화 검색 API
 def search_naver(keyword):
-   query = urllib.parse.quote(keyword)
-   url = NMV + query
+    query = urllib.parse.quote(keyword)
+    url = NMV + query
 
-   request_movie = urllib.request.Request(url)
-   request_movie.add_header("X-Naver-Client-Id", CID)
-   request_movie.add_header("X-Naver-Client-Secret", CSC)
-   response = urllib.request.urlopen(request_movie)
-   
-   rescode = response.getcode()
-   if rescode==200:
-      result = []
-      items = json.loads(response.read().decode("utf-8"))["items"]
-      for item in items:
-         summary = {
+    request_movie = urllib.request.Request(url)
+    request_movie.add_header("X-Naver-Client-Id", CID)
+    request_movie.add_header("X-Naver-Client-Secret", CSC)
+    response = urllib.request.urlopen(request_movie)
+
+    rescode = response.getcode()
+    if rescode==200:
+        result = []
+        items = json.loads(response.read().decode("utf-8"))["items"]
+        for item in items:
+            summary = {
             "title": remove_tags(item["title"]),
-            "code": item["link"].split("?code=")[1],
+            "code": int(item["link"].split("?code=")[1]),
+            "image": item["image"],
             "director": item["director"].strip("|"),
+            "actor": item["actor"].strip("|"),
             "pubDate": item["pubDate"],
-         }
-         result.append(summary)
-      return jsonify({ result })
-   else:
-      return print(f"Error Code: {rescode}")
+            "naverRating": item["userRating"],
+            }
+            result.append(summary)
+        movie_add(result[0:5])
+        return result
+    else:
+        return print(f"Error Code: {rescode}")
 
 
+# 네이버영화API 검색 결과 중 상위 5개만 DB에 등록
+def movie_add(movies):
+    for movie in movies:
+        title = movie["title"]
+        code = movie["code"]
+
+        url = f"https://movie.naver.com/movie/bi/mi/basic.naver?code={code}"
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        data = requests.get(url, headers=headers)
+        soup = BeautifulSoup(data.text, 'html.parser')
+        desc = soup.select_one("#content > div.article > div.section_group.section_group_frst > div:nth-child(1) > div > div.story_area > p")
+
+        movie = {
+            "code": code,
+            "image": movie["image"],
+            "title": title,
+            "director": movie["director"],
+            "actor": movie["actor"],
+            "pubDate": movie["pubDate"],
+            "naverRating": movie["naverRating"],
+            "userRating": "0.00",
+            "description": remove_tags(str(desc)),
+            "reviews": [],
+        }
+        find = db.movies.find_one({"code": code})
+
+        cnt = 0
+        if find is None:
+            db.movies.insert_one(movie)
+            cnt += 1
+    
+    return f"{cnt} movies added to DB"
 
 
 @movie_bp.route("/test", methods=["GET"])
@@ -100,6 +146,60 @@ def test():
     "name": "kim",
     "birth": 1994,
     "skills": ["node.js", "python3"]
-   }
-   
+   }   
    return render_template("TESTPAGE.html", a=a)
+
+
+@movie_bp.route("/test2")
+def test2():
+    return ""
+
+
+
+# 네이버 영화DB 스크랩 -> DB 유지관리용. 웹사이트에는 사용 안될거에요
+# @movie_bp.route("/scrap", methods=["GET"])
+def scrap():
+    print("scrap")
+
+    url = "https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=cnt&tg=0&date=20210625"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # #old_content > table > tbody > tr:nth-child(2) > td.title
+    body = soup.select_one("#old_content > table > tbody")
+    # old_content > table > tbody > tr:nth-child(2) > td.title > div > a
+    tags = body.select("tr > td.title > div > a")
+    for tag in tags:
+        title = tag["title"]
+        code = int(tag["href"].split("code=")[1])
+
+        naver = search_naver(title)[0]
+
+        url = f"https://movie.naver.com/movie/bi/mi/basic.naver?code={code}"
+        data = requests.get(url, headers=headers)
+        soup = BeautifulSoup(data.text, 'html.parser')
+        desc = soup.select_one("#content > div.article > div.section_group.section_group_frst > div:nth-child(1) > div > div.story_area > p")
+
+        movie = {
+            "code": code,
+            "image": naver["image"],
+            "title": title,
+            "director": naver["director"],
+            "actor": naver["actor"],
+            "pubDate": naver["pubDate"],
+            "naverRating": naver["naverRating"],
+            "userRating": "0.00",
+            "description": remove_tags(str(desc)),
+            "reviews": [],
+        }
+        find = db.movies.find_one({"code": code})
+        # print(movie)
+        if find is None:
+            print(f"{title} None")
+            db.movies.insert_one(movie)
+        else:
+            print(f"{title} Exists")
+
+    return ""
