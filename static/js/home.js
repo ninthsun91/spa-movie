9db7f4b2be7c195314c43c121d7f9f460591dbdf
obov <obov3e@gmail.com>
2022-09-07 01:53:20 +0900

    slug

--- static/js/home.js
+++ static/js/home.js
@@ -1,14 +1,29 @@
 const leftChevron = () => $("#leftChevron");
 const rightChevron = () => $("#rightChevron");
-const imgbox = () => $(".img-box");
+const imgbox = () => $(".carousel-box");
 const carouselContainer = () => $(".carousel-constainer");
-const lastImgBox = () => $(".carousel-constainer .img-box:last-child");
-const firstImgBox = () => $(".carousel-constainer .img-box:first-child");
+const lastImgBox = () => $(".carousel-constainer .carousel-box:last-child");
+const firstImgBox = () => $(".carousel-constainer .carousel-box:first-child");
+const indicator = () => $(".indicator");
+const slugs = (number) => $(`.slug:nth-child(${number})`);
+
+let numberIndicating = 0;
 let clickable = true;
+const calculating = (num) => (((num % 3) + 3) % 3) + 1;
+const shrinkSlug = function (num) {
+  slugs(calculating(num)).css("width", "15px");
+};
 const handleClickLeftChevron = function () {
   if (clickable) {
     clickable = false;
-    imgbox().css("transition", "all 1s");
+    const copyNumber = numberIndicating;
+    setTimeout(function () {
+      shrinkSlug(copyNumber);
+    }, 500);
+    numberIndicating -= 1;
+    slugs(calculating(numberIndicating)).css("width", "50px");
+
+    imgbox().css("transition", "all 0.5s");
     imgbox().css("transform", `translateX(100vw)`);
     setTimeout(function () {
       imgbox().css("transition", "none");
@@ -22,7 +37,14 @@ const handleClickLeftChevron = function () {
 const handleClickRightChevron = function () {
   if (clickable) {
     clickable = false;
-    imgbox().css("transition", "all 1s");
+    const copyNumber = numberIndicating;
+    setTimeout(function () {
+      shrinkSlug(copyNumber);
+    }, 500);
+    numberIndicating += 1;
+    slugs(calculating(numberIndicating)).css("width", "50px");
+
+    imgbox().css("transition", "all 0.5s");
     imgbox().css("transform", `translateX(-100vw)`);
     setTimeout(function () {
       imgbox().css("transition", "none");
