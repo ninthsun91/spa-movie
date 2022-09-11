483d0a6290700e067fed6dbf0dba395de085073f
obov <obov3e@gmail.com>
2022-09-11 12:37:22 +0900

    login/ logout / review login block

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 from .config.session import *
 from .database import user_uid
-
+from .util import *
 
 main_bp = Blueprint("main", __name__)
 
@@ -10,8 +10,11 @@ main_bp = Blueprint("main", __name__)
 @main_bp.route("/")
 def home():
     initialize_home_session()
-    return render_template("home.html")
-
+    if(token_check() == "로그인 세션이 만료되었습니다.") :
+        return render_template("home.html",session="out")
+    else :
+        return render_template("home.html",session="in")
+        
 
 @main_bp.route("/rev")
 def review():
--- frontend/static/js/modules/router.js
+++ frontend/static/js/modules/router.js
@@ -1,79 +1,79 @@
 const loadPage = (pathname, complete = undefined) => {
-    $("#App").load(`${pathname} #AppContainer`, complete);
+  $("#App").load(`${pathname} #AppContainer`, complete);
 };
 
-const pushHistory = (pathname) => history.pushState({pathname}, "", location.origin + pathname);
-history.replaceState({pathname: location.pathname}, "");
-window.onpopstate = function ({state}) {
-    switch (state.pathname) {
-        case PATH_NAME.REV:
-            return loadPage(PATH_NAME.REV);
-        case PATH_NAME.HOME:
-            return loadPage(PATH_NAME.HOME);
-    }
+const pushHistory = (pathname) => history.pushState({ pathname }, "", location.origin + pathname);
+history.replaceState({ pathname: location.pathname }, "");
+window.onpopstate = function ({ state }) {
+  switch (state.pathname) {
+    case PATH_NAME.REV:
+      return loadPage(PATH_NAME.REV);
+    case PATH_NAME.HOME:
+      return loadPage(PATH_NAME.HOME);
+  }
 };
 
 const reloadPage = function (pathName, handler) {
-    const isAtTop = window.scrollY < 90;
-    const isPathSame = location.pathname === pathName;
-    if (isAtTop && isPathSame) {
-    }
-    if (!isAtTop && isPathSame) {
-        window.scrollTo(0, 0);
-    }
-    if (isAtTop && !isPathSame) {
-        pushHistory(pathName);
-        loadPage(pathName, handler);
-    }
-    if (!isAtTop && !isPathSame) {
-        window.scrollTo(0, 0);
-        setTimeout(function () {
-            pushHistory(pathName);
-            loadPage(pathName, handler);
-        }, 1000);
-    }
+  const isAtTop = window.scrollY < 90;
+  const isPathSame = location.pathname === pathName;
+  if (isAtTop && isPathSame) {
+  }
+  if (!isAtTop && isPathSame) {
+    window.scrollTo(0, 0);
+  }
+  if (isAtTop && !isPathSame) {
+    pushHistory(pathName);
+    loadPage(pathName, handler);
+  }
+  if (!isAtTop && !isPathSame) {
+    window.scrollTo(0, 0);
+    setTimeout(function () {
+      pushHistory(pathName);
+      loadPage(pathName, handler);
+    }, 1000);
+  }
 };
 
 //my page
 const loadMyPage = function () {
-    window.scrollTo(0, 0);
-    if (location.pathname === PATH_NAME.PROFILE) {
-    } else {
-        setTimeout(function () {
-            pushHistory(PATH_NAME.PROFILE);
-            loadPage(PATH_NAME.PROFILE, handleLoadMyPage);
-        }, 1000);
-    }
+  window.scrollTo(0, 0);
+  if (location.pathname === PATH_NAME.PROFILE) {
+  } else {
+    setTimeout(function () {
+      pushHistory(PATH_NAME.PROFILE);
+      loadPage(PATH_NAME.PROFILE, handleLoadMyPage);
+    }, 1000);
+  }
 };
 
 const handleLoadMyPage = function () {
-    console.log("after load my page");
-    setTitle(TITLE.MY_PAGE);
-}
+  console.log("after load my page");
+  setTitle(TITLE.MY_PAGE);
+};
 
 const handleLoadHome = function () {
-    console.log("after load home");
+  console.log("after load home");
 
-    setTitle(TITLE.HOME);
-    loadComponent("movieListNow", "/components/postercard?direction=vertical&count=5&type=now");
-    loadComponent("movieListTrending", "/components/postercard?direction=vertical&count=5&type=trend");
-    numberIndicating = 0;
+  setTitle(TITLE.HOME);
+  loadComponent("movieListNow", "/components/postercard?direction=vertical&count=5&type=now");
+  loadComponent("movieListTrending", "/components/postercard?direction=vertical&count=5&type=trend");
+  numberIndicating = 0;
 };
 const handleLoadRev = function () {
-    console.log("after load rev");
-    setTitle(TITLE.REV);
-    loadComponent("recentReview", "/components/reviewcard?type=recent");
-    loadComponent("popularReview", "/components/reviewcard?type=popular");
-    setTimeout(function () {
-        reviewMenuSlideUp();
-        reviewContainerWidthGrow();
-    }, 300);
+  console.log("after load rev");
+  setTitle(TITLE.REV);
+  loadComponent("recentReview", "/components/reviewcard?type=recent");
+  loadComponent("popularReview", "/components/reviewcard?type=popular");
+  setTimeout(function () {
+    reviewMenuSlideUp();
+    reviewContainerWidthGrow();
+  }, 300);
 };
 
 const loadComponent = function (tagId, pathname, complete = undefined) {
-    $("#" + tagId).load(`${pathname}`, complete, ()=>console.log(`${tagId} loaded`));
+  $("#" + tagId).load(`${pathname}`, complete, () => console.log(`${tagId} loaded`));
 };
 
 const setTitle = function (title) {
-    document.title = title + " | Movie Toy";
+  document.title = title + " | Movie Toy";
 };
